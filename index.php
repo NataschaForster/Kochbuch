c73eee45d6a02ea3dc498523f9e654bbf40c22a4
Felix <felixkeller98@gmail.com>
2019-11-17 23:15:02 +0100

    fixed edit and reload issue

--- index.php
+++ index.php
@@ -1,67 +1,68 @@
-<?php 
-  session_start(); 
+<?php
+session_start();
+require_once('comments.php');
 
-  if (!isset($_SESSION['username'])) {
-  	$_SESSION['msg'] = "You must log in first";
-  	header('location: login.php');
-  }
-  if (isset($_GET['logout'])) {
-  	session_destroy();
-  	unset($_SESSION['username']);
-  	header("location: login.php");
-  }
+if (!isset($_SESSION['username'])) {
+	$_SESSION['msg'] = "You must log in first";
+	header('location: login.php');
+}
+if (isset($_GET['logout'])) {
+	session_destroy();
+	unset($_SESSION['username']);
+	header("location: login.php");
+}
 
-  require_once('comments.php');
 ?>
 <!DOCTYPE html>
 <html>
-<head>	
+
+<head>
 	<!-- Required meta tags always come first -->
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<meta http-equiv="x-ua-compatible" content="ie=edge">
 	<!-- Bootstrap CSS -->
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-	
-	<link rel="stylesheet" href="../full-width-pics.css"/>
+
+	<link rel="stylesheet" href="../full-width-pics.css" />
 	<title>Home</title>
 	<link rel="stylesheet" type="text/css" href="style.css">
-	<link rel="stylesheet" href="../main.css"/>
+	<link rel="stylesheet" href="../main.css" />
 </head>
 
 <body>
 
-<!-- NAVIGATION BAR AND BURGER MENUE -->
+	<!-- NAVIGATION BAR AND BURGER MENUE -->
 	<nav class="navbar navbar-fixed-top navbar-dark bg-dark shadow navbar-expand-md">
 		<div class="container">
 			<a class="navbar-brand" href="index.php">Recipe book</a>
 			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="toggle navigation">
 				<span class="navbar-toggler-icon"></span>
 			</button>
-			
+
 			<div class="collapse navbar-collapse" id="navbarSupportedContent">
 				<ul class="navbar-nav ml-auto">
-				<!-- logged in user information -->
-					<?php  if (isset($_SESSION['username'])) : ?>
-						<li class="nav-item px-4 py-0" style="height: 20px"> 
-							<a class="nav-link" href="index.php?logout='1'">Logout</a> 
+					<!-- logged in user information -->
+					<?php if (isset($_SESSION['username'])) : ?>
+						<li class="nav-item px-4 py-0" style="height: 20px">
+							<a class="nav-link" href="index.php?logout='1'">Logout</a>
 						</li>
-					<?php endif ?>					
+					<?php endif ?>
 				</ul>
 			</div>
 		</div>
 	</nav>
 
-	
-</div>
 
-<!-- HEADER IMAGE -->
+	</div>
+
+	<!-- HEADER IMAGE -->
 	<header class="py-5 bg-image-full" style="background-image: url('/Bilder/cooking.jpg');">
 		<div class="row ml-5" style="height:400px">
 			<h1 class="d-flex align-items-end" id="batch">My favorite recipes</h1>
 		</div>
 	</header>
-	
+
 	<!-- FIRST CONTENT -->
 	<div class="container">
 		<div class="row">
@@ -72,10 +73,10 @@
 			<div class="col-xs-12 col-md-6 my-5">
 				<h3>Some pictures I drew.</h3>
 				<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
-			</div>	
+			</div>
 		</div>
 	</div>
-	
+
 	<!-- COOKING CONTENT -->
 	<div class="container">
 		<div class="row">
@@ -89,7 +90,7 @@
 				<table id="1">
 					<tbody>
 						<tr>
-							<td class="pr-1" >1</td>
+							<td class="pr-1">1</td>
 							<td> cups of rice</td>
 						</tr>
 						<tr>
@@ -126,16 +127,17 @@
 			</div>
 			<div class="col-md-6 col-lg-4">
 				<p id="ingredients">Instruction</p>
-				<p>Cook rice according to instruction. I mostly use 3 cups of water and a little salt. <break/> 
-				Chop onions and tomatoes. Fry onions. Add tomatoes and a splash of water to the pan. Turn down on medium heat and cover the pan with a lid. Add spices.
-				Heat water in a water boiler. Add peanutbutter and a little water in a bowl. Stir until creamy. Add more water if needed. Combine everything and enjoy!
+				<p>Cook rice according to instruction. I mostly use 3 cups of water and a little salt.
+					<break />
+					Chop onions and tomatoes. Fry onions. Add tomatoes and a splash of water to the pan. Turn down on medium heat and cover the pan with a lid. Add spices.
+					Heat water in a water boiler. Add peanutbutter and a little water in a bowl. Stir until creamy. Add more water if needed. Combine everything and enjoy!
 				</p>
 			</div>
 			<div class="col-xs-12 col-lg-4 offset-lg-1">
 				<img class="img-fluid max-width:100% height:auto" src="/Bilder/peanut.jpg">
 			</div>
 		</div>
-		
+
 		<!-- Comment Section -->
 		<div class="row pt-4">
 			<div class="col-md-6 col-md-offset-3">
@@ -144,7 +146,7 @@
 						<form id="comment" action="index.php" method="POST">
 							<div class="form-group">
 								<textarea name="comment" placeholder="Write your comment here!" class="pb-cmnt-textarea"></textarea>
-								<input name="username" type="text" value="<?php echo $_SESSION['username']?>" hidden></input>
+								<input name="username" type="text" value="<?php echo $_SESSION['username'] ?>" hidden></input>
 								<input name="recipe" type="text" value="1" hidden></input>
 								<button name="submit_comment" class="btn btn-primary pull-right" type="submit">Share</button>
 							</div>
@@ -160,7 +162,7 @@
 				<div class="card-header" id="headingOne">
 					<h2 class="mb-0">
 						<button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
-						Show Comments
+							Show Comments
 						</button>
 					</h2>
 				</div>
@@ -168,45 +170,45 @@
 				<div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordionExample">
 					<div class="card-body">
 						<table class="table">
-							<?php foreach($result_1 as $row): ?>
-							<tr>
-								<td><?php echo $row['username']; ?></td>
-								<td><?php echo $row['comment']; ?></td>
-								<?php if($row['username'] == $_SESSION['username']): ?>
-									
-									<!-- Trigger the modal with a button -->
-									<td><button type="button" class="btn" data-toggle="modal" data-target="#myModal"><img src="https://img.icons8.com/ios-glyphs/24/000000/edit.png"></button></td>
+							<?php foreach ($result_1 as $row) : ?>
+								<tr>
+									<td><?php echo $row['username']; ?></td>
+									<td><?php echo $row['comment']; ?></td>
+									<?php if ($row['username'] == $_SESSION['username']) : ?>
 
-									<form id="delete" action="index.php" method="POST">
+										<!-- Trigger the modal with a button -->
+										<td><button type="button" class="btn" data-toggle="modal" data-target="#myModal-<?php echo $row['id']; ?>"><img src="https://img.icons8.com/ios-glyphs/24/000000/edit.png"></button></td>
 
-										<input name="id" type="text" value="<?php echo $row['id']; ?>" hidden></input>
-									
-										<td> <button type="submit" name="delete_comment"> <img src="https://img.icons8.com/metro/26/000000/delete.png"> </button> </td>
-									</form>
-														
-									<!-- Modal -->
-									<div class="modal fade" id="myModal" role="dialog">
-										<div class="modal-dialog">
-										
-											<!-- Modal content-->
-											<div class="modal-content">
-												<div class="modal-header">
-													<h3>Edit your comment here.</h3>
-													<button type="button" class="close" data-dismiss="modal">&times;</button>
-												</div>
-												<div class="modal-body">
-													<form id="modal_form" class="m-0 border-0" action="index.php" method="POST">
-														<input type="text" name="updated_comment" value="<?php echo $row['comment']; ?>"></input>
-														<button name ="update_comment" type="submit" class="btn btn-default">Save changes</button>
-														<input name="id" type="text" value="<?php echo $row['id']; ?>" hidden></input>
-													</form>
+										<form id="delete" action="index.php" method="POST">
+
+											<input name="id" type="text" value="<?php echo $row['id']; ?>" hidden></input>
+
+											<td> <button type="submit" name="delete_comment"> <img src="https://img.icons8.com/metro/26/000000/delete.png"> </button> </td>
+										</form>
+
+										<!-- Modal -->
+										<div class="modal fade" id="myModal-<?php echo $row['id']; ?>" role="dialog">
+											<div class="modal-dialog">
+
+												<!-- Modal content-->
+												<div class="modal-content">
+													<div class="modal-header">
+														<h3>Edit your comment here.</h3>
+														<button type="button" class="close" data-dismiss="modal">&times;</button>
+													</div>
+													<div class="modal-body">
+														<form id="modal_form" class="m-0 border-0" action="index.php" method="POST">
+															<input name="id" type="text" value="<?php echo $row['id']; ?>" hidden></input>
+															<input type="text" name="updated_comment" value="<?php echo $row['comment']; ?>"></input>
+															<button name="update_comment" type="submit" class="btn btn-default">Save changes</button>
+														</form>
+													</div>
 												</div>
 											</div>
 										</div>
-									</div>
 
-								<?php endif; ?>
-							</tr>
+									<?php endif; ?>
+								</tr>
 							<?php endforeach; ?>
 						</table>
 					</div>
@@ -215,49 +217,49 @@
 		</div>
 
 		<!-- COOKING CONTENT -->
-			<div class="row">
-				<div class="col my-5 pt-5">
-					<h3>Cherry Tomato Spaghetti</h3>
-				</div>
+		<div class="row">
+			<div class="col my-5 pt-5">
+				<h3>Cherry Tomato Spaghetti</h3>
 			</div>
-			<div class="row">
-				<div class="col-xs-12 col-md-6 col-lg-3">
-					<p id="ingredients">Ingredients</p>
-					<table id="2">
-						<tbody>
-							<tr>
-								<td class="pr-1" >25</td>
-								<td> cherry tomatoes</td>
-							</tr>
-							<tr>
-								<td class="pr-1">500</td>
-								<td> gr spaghetti</td>
-							</tr>
-							<tr>
-								<td class="pr-1">1</td>
-								<td> tbs butter</td>
-							</tr>
-							<tr>
-								<td class="pr-1"></td>
-								<td> garlic powder</td>
-							</tr>
-							<tr>
-								<td class="pr-1"></td>
-								<td> salt</td>
-							</tr>
-							<tr>
-								<td class="pr-1"></td>
-								<td> pepper</td>
-							</tr>
-							<tr>
-								<td class="pr-1"></td>
-								<td> chilli powder</td>
-							</tr>
-						</tbody>
-					</table>
-					<p>For <input type="number" id="number_field" value="2" onClick="calculate(2, number_field)"> servings</p>
-					</form>				
-				</div>
+		</div>
+		<div class="row">
+			<div class="col-xs-12 col-md-6 col-lg-3">
+				<p id="ingredients">Ingredients</p>
+				<table id="2">
+					<tbody>
+						<tr>
+							<td class="pr-1">25</td>
+							<td> cherry tomatoes</td>
+						</tr>
+						<tr>
+							<td class="pr-1">500</td>
+							<td> gr spaghetti</td>
+						</tr>
+						<tr>
+							<td class="pr-1">1</td>
+							<td> tbs butter</td>
+						</tr>
+						<tr>
+							<td class="pr-1"></td>
+							<td> garlic powder</td>
+						</tr>
+						<tr>
+							<td class="pr-1"></td>
+							<td> salt</td>
+						</tr>
+						<tr>
+							<td class="pr-1"></td>
+							<td> pepper</td>
+						</tr>
+						<tr>
+							<td class="pr-1"></td>
+							<td> chilli powder</td>
+						</tr>
+					</tbody>
+				</table>
+				<p>For <input type="number" id="number_field" value="2" onClick="calculate(2, number_field)"> servings</p>
+				</form>
+			</div>
 			<div class="col-md-6 col-lg-4">
 				<p id="ingredients">Instruction</p>
 				<p>Cook spaghetti. Heat butter in pan, add garlic powder. Add tomatoes and cover with lid. Wait for 10mins and Smash tomatoes. Add a little extra oil. Mix spaghetti and sauce. Enjoy!</p>
@@ -266,7 +268,7 @@
 				<img class="img-fluid max-width:100% height:auto" src="/Bilder/Cherry.jpg">
 			</div>
 		</div>
-		
+
 		<!-- Comment Section -->
 		<div class="row pt-4">
 			<div class="col-md-6 col-md-offset-3">
@@ -290,7 +292,7 @@
 				<table id="3">
 					<tbody>
 						<tr>
-							<td class="pr-1" ></td>
+							<td class="pr-1"></td>
 							<td> lasagna noodles</td>
 						</tr>
 						<tr>
@@ -323,7 +325,7 @@
 				<table id="4">
 					<tbody>
 						<tr>
-							<td class="pr-1" >1</td>
+							<td class="pr-1">1</td>
 							<td> tbsp nutritional yeast</td>
 						</tr>
 						<tr>
@@ -349,8 +351,8 @@
 					</tbody>
 				</table>
 				<p class="py-3">
-					For <input type="number" id="number_field" value="4"  onClick="calculate(3, number_field)"> servings</input>
-				</p>				
+					For <input type="number" id="number_field" value="4" onClick="calculate(3, number_field)"> servings</input>
+				</p>
 			</div>
 			<div class="col-md-6 col-lg-4">
 				<p id="ingredients">Instruction</p>
@@ -360,7 +362,7 @@
 				<img class="img-fluid max-width:100% height:auto" src="/Bilder/lasagna.jpg">
 			</div>
 		</div>
-		
+
 		<!-- Comment Section -->
 		<div class="row pt-4">
 			<div class="col-md-6 col-md-offset-3">
@@ -384,7 +386,7 @@
 				<table id="2">
 					<tbody>
 						<tr>
-							<td class="pr-1" >1</td>
+							<td class="pr-1">1</td>
 							<td> can coconut milk</td>
 						</tr>
 						<tr>
@@ -414,8 +416,8 @@
 					</tbody>
 				</table>
 				<p class="py-3">
-					For <input type="number" id="number_field" value="2"  onClick="calculate(4, number_field)"> servings</input>
-				</p>				
+					For <input type="number" id="number_field" value="2" onClick="calculate(4, number_field)"> servings</input>
+				</p>
 			</div>
 			<div class="col-md-6 col-lg-4">
 				<p id="ingredients">Instruction</p>
@@ -439,16 +441,17 @@
 		</div>
 
 	</div>
-		
+
 	<!-- jQuery first, then Bootstrap JS. -->
 	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>	
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 	<script src="/calculate.js"></script>
 	<script>
-	function myFunction() {
-	  alert($_SESSION['success']);
-	}
+		function myFunction() {
+			alert($_SESSION['success']);
+		}
 	</script>
 </body>
+
 </html>
